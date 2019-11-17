644ae27367d916243d29b98df64e720bc1c0e696
Rajat Kanti Nandi <rajatkantinandi1993@gmail.com>
2019-11-18 01:39:41 +0530

    Code refactoring & custom components

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import WeatherNow from './Components/WeatherNow';
-import DayWeather from './Components/DayWeather';
+import WeekWeather from './Components/WeekWeather';
 import { fromCtoF, fromFtoC } from './helpers/tempHelper';
 import useFetchTemp from './hooks/useFetchTemp';
 import useBgImageURL from './hooks/useBgImageURL';
@@ -19,11 +19,6 @@ export default function App(props) {
   const bgImageURL = useBgImageURL(weather.id);
   const [upcomingFilter, setUpcomingFilter] = useState(null);
   const [mainTransform, setTransform] = useState(null);
-
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-  days = [...days, ...days];
-  const today = new Date().getDay();
-  days = days.slice(today + 1, today + 5);
 
   const handleUnitChange = () => {
     if (temp !== '---') {
@@ -45,12 +40,7 @@ export default function App(props) {
     <Header {...{ locationName, unit, handleUnitChange }} />
     <main style={{ background: bgImageURL }}>
       <WeatherNow {...{ temp, temps, unit, weather, setUpcomingFilter, mainTransform, setTransform }} />
-      <section className="weather-coming" style={{ filter: upcomingFilter, transform: mainTransform }}>
-        <h2>This Week</h2>
-        <div className="days">
-          {days.map((day, idx) => <DayWeather {...{ day, unit }} temp={temps[idx + 1]} weather={comingWeather[idx + 1]} key={idx} />)}
-        </div>
-      </section>
+      <WeekWeather {...{ temps, unit, comingWeather, upcomingFilter, mainTransform }} />
     </main>
     <Footer {...{ location }} />
   </Fragment>;
