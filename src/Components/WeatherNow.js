9c998e2fcedc1ab89a291542e7b480336606bd2a
Rajat Kanti Nandi <rajatkantinandi1993@gmail.com>
2019-11-24 01:32:09 +0530

    style fixes

--- src/Components/WeatherNow.js
+++ src/Components/WeatherNow.js
@@ -14,13 +14,17 @@ export default function WeatherNow({ temp, unit, weather, temps, setUpcomingFilt
             <h2>Now</h2>
             <h2>{temp} {unit}</h2>
             <h2>{weather.description}</h2>
-            <h3><span className="arrow up"></span>
-                Max: {temps[0][0]} {unit} &nbsp;
-        <span className="arrow down"></span>
-                Min: {temps[0][1]} {unit}
+            <h3>
+                <span className="arrow up"></span> Max: {temps[0][0]}{unit} &nbsp;
+                <span className="arrow down"></span> Min: {temps[0][1]}{unit}
             </h3>
             <h4>Humidity: {weather.humidity}%, Wind Speed: {weather.windSpeed}m/s</h4>
-            {aqi && <h4>AQI({cityName}): <span className={'aqi ' + getAQIClass(aqi)}>{aqi}</span></h4>}
+            {aqi &&
+                <h4 className="aqiData">
+                    <div>Air Quality Index: <span className={'aqi ' + getAQIClass(aqi)}>{aqi}</span></div>
+                    <div>({cityName})</div>
+                </h4>
+            }
             <button className="show-coming" onClick={handleComing} style={{ display: comingBtnDisplay }}>Show Next Days</button>
         </section>
     )
