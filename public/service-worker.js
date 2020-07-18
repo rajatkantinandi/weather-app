20e157f7e6b665822860524f432d81eaba29e1bf
Rajat Kanti Nandi <rajatkantinandi1993@gmail.com>
2020-07-18 20:56:47 +0530

    add version.json to network first strategy

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,6 +5,7 @@ if (workbox) {
   workbox.routing.registerRoute(new RegExp('/images/backgrounds/'), new workbox.strategies.StaleWhileRevalidate());
   workbox.routing.registerRoute(new RegExp('/favicon.png'), new workbox.strategies.StaleWhileRevalidate());
   workbox.routing.registerRoute(/\.(?:js|css|html)$/, new workbox.strategies.StaleWhileRevalidate());
+  workbox.routing.registerRoute('/version.json', new workbox.strategies.NetworkFirst());
 } else {
   console.log(`Boo! Workbox didn't load 😬`);
 }
